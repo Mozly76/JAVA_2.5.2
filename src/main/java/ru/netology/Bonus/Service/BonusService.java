03586d7e572fe5b3c7027056e3a07c46f91d757f
Sergey Kiselev <mozly76@yandex.ru>
2022-07-04 22:23:54 +0300

    Adding CsvFileSource

--- src/main/java/ru/netology/Bonus/Service/BonusService.java
+++ src/main/java/ru/netology/Bonus/Service/BonusService.java
@@ -1,3 +1,5 @@
+package ru.netology.Bonus.Service;
+
 public class BonusService {
     public long calculate(long amount, boolean registered) {
         int percent = registered ? 3 : 1;
---
+++ src/test/java/ru/netology/Bonus/Service/BonusServiceTest.java
@@ -0,0 +1,21 @@
+package ru.netology.Bonus.Service;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BonusServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/data.csv")
+
+    public void shouldCalculate(long amount, boolean registered, long expected) {
+        BonusService service = new BonusService();
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+}
